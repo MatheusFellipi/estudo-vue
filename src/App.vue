39bbdd24423efdd12e3dae91e5fe073ed631f50e
Matheus Fellipi <matheus.fellipi@outlook.com>
2022-07-13 21:12:04 -0300

    estudos

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div v-for="(clientes,inidex) in clientes" :key="clientes.id">
+     {{inidex}}
+      <Cliente :clientes="clientes" :verIdade="true" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Cliente from "./components/Cliente";
+export default {
+  name: "App",
+  data() {
+    return {
+      clientes: [
+        {
+          id:1,
+          nome: "isso",
+          email: "tesste@",
+          numero: "456",
+          idade: "1",
+        },
+        {
+          id:2,
+          nome: "isso",
+          email: "tesste@",
+          numero: "456",
+          idade: "1",
+        },
+        {
+          id:3,
+          nome: "isso",
+          email: "tesste@",
+          numero: "456",
+          idade: "1",
+        },
+      ],
+    };
+  },
+  components: {
+    Cliente,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="{ clinte: !isPreminm, cliente_prime: isPreminm }">
+    <h4>Nome: {{ clientes.nome }}</h4>
+    <hr />
+    <hr />
+    <p>Numero: {{ clientes.numero }}</p>
+    <p>Email: {{ clientes.email }}</p>
+    <p v-if="verIdade">Idade: {{ clientes.idade }}</p>
+    <p v-else>Idade: nao tenho</p>
+    <button @click="mudarcor">mudar de cor</button>
+  </div>
+</template> 
+<script>
+export default {
+  data() {
+    return { isPreminm: false };
+  },
+  props: {
+    clientes: Object,
+    verIdade: Boolean,
+  },
+  methods: {
+    mudarcor: function () {
+      this.isPreminm = !this.isPreminm;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.cliente {
+  background-color: #f5f5fd;
+  padding: 1rem;
+  height: 200px;
+  max-width: 350px;
+  margin-top: 1rem;
+}
+.cliente_prime {
+  background-color: #0000ff;
+  padding: 1rem;
+  height: 200px;
+  max-width: 350px;
+  margin-top: 1rem;
+}
+</style>
